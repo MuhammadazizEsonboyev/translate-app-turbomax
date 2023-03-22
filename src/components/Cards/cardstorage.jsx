d6e679a4caf84e8b288ebd7d664459b8e6263506
unknown <umirzakovasad@gmail.com>
2023-03-22 19:28:10 +0500

    asa

--- src/components/Cards/cardstorage.jsx
+++ src/components/Cards/cardstorage.jsx
@@ -1,36 +1,38 @@
 import React from 'react'
 import { Button, Card, Col, Container, Row } from 'react-bootstrap'
+import { useTranslation } from 'react-i18next'
 
 const Cardstorage = () => {
+  const { t } = useTranslation()
   const data = [
     {
       img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Asadbek",
-      info: '04 turbo max',
+      title: `${t("text.titleOfCountries")}`,
+      info: 'The material world of the Universe, in essence, is the main object of study of the natural sciences. In everyday life, the word "nature" is often used in the sense of a natural habitat.',
       id: 1
     },
     {
-      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Doston",
-      info: '04 turbo max',
+      img: "https://www.japan-guide.com/g6/XYZeXYZe2172_225.jpg",
+      title: "Nature of Japan",
+      info: 'Many visitors to Japan limit their sightseeing activities to the country`s heavily urbanized areas between Tokyo, Kyoto and Hiroshima.',
       id: 1
     },
     {
-      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Asadbek",
-      info: '04 turbo max',
+      img: "https://uzbekistan.travel/storage/app/media/cropped-images/5952701_xlarge-0-0-0-0-1584517453.jpg",
+      title: "Nature of Uzbekistan",
+      info: 'Uzbekistan has no access to the sea and makes up for this with a huge number of the world’s most beautiful lakes, natural and artificial reservoirs and large rivers that cover the entire Central Asia.',
       id: 1
     },
     {
-      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Doston",
-      info: '04 turbo max',
+      img: "https://www.advantour.com/img/kazakhstan/images/nature.jpg",
+      title: "Nature of Kazakhstan",
+      info: 'Kazakhstan has 9th the largest territory in the world (2,724,900km2), with the population of only 17 million',
       id: 1
     },
     {
-      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Asadbek",
-      info: '04 turbo max',
+      img: "https://newsroom.korea.net/upload/content/editImage/20201230202905720_VBDSHY2C.png",
+      title: "Nature of Korea",
+      info: 'South Korea has designated and operated national parks to preserve the natural ecosystems and natural and cultural wonders.',
       id: 1
     },
     {
@@ -51,15 +53,6 @@ const Cardstorage = () => {
       info: '04 turbo max',
       id: 1
     }
-    ,
-    {
-      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
-      title: "Doston",
-      info: '04 turbo max',
-      id: 1
-    }
-
-
   ]
   return (
     <div>
