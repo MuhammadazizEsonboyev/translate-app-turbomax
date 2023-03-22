c398ca432fa9b36138cd17209ccb34dc4240cf16
QodirovDoston <qodirovdoston806@gmail.com>
2023-03-22 19:04:29 +0500

    dfd

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
       <h1>Hello world</h1>
       <NavbarPage/>
       <Cardstorage/>
+    
+  
     </>
   );
 }
--- src/components/Cards/cardstorage.jsx
+++ src/components/Cards/cardstorage.jsx
@@ -1,29 +1,92 @@
 import React from 'react'
+import { Button, Card, Col, Container, Row } from 'react-bootstrap'
 
 const Cardstorage = () => {
-const data=[
+  const data = [
     {
-        img:"",
-        title:"dostomdhufhfdp",
-        info:'04 turbo max',
-        id:1
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Asadbek",
+      info: '04 turbo max',
+      id: 1
     },
     {
-        img:"",
-        title:"dostomdhufhfdp",
-        info:'04 turbo max',
-        id:1
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Doston",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Asadbek",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Doston",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Asadbek",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Doston",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Asadbek",
+      info: '04 turbo max',
+      id: 1
+    },
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Doston",
+      info: '04 turbo max',
+      id: 1
+    }
+    ,
+    {
+      img: "https://wallpapers.com/images/featured/banana-leaf-i1m1ulaygn46l916.jpg",
+      title: "Doston",
+      info: '04 turbo max',
+      id: 1
     }
 
-    ]
+
+  ]
   return (
     <div>
-      {data.map((value)=>{
-        <div>
-            <img src={value.img} alt="" />
-            <p>{value.title}</p>
-        </div>
-      })}
+      <Container>
+        <Row className='justify-content-center mt-5 '>
+          {data.map((value) => {
+            return (
+              <>
+                <Col sx={3} className="mb-5">
+                  <div>
+                    <Card style={{ width: '18rem' }}>
+                      <Card.Img variant="top" src={value.img} />
+                      <Card.Body>
+                        <Card.Title>{value.title}</Card.Title>
+                        <Card.Text>
+                          {value.info}
+                        </Card.Text>
+                        <Button variant="primary">Go somewhere</Button>
+                      </Card.Body>
+                    </Card>
+                  </div>
+                </Col>
+              </>
+            )
+          })}
+        </Row>
+      </Container>
     </div>
   )
 }
