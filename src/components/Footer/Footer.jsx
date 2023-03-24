ea9b58e32e919e52301b79c126d22e7d2ec89559
unknown <umirzakovasad@gmail.com>
2023-03-24 18:00:59 +0500

    asad/fixed

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -12,7 +12,6 @@ export default function Footer() {
         <>
             <section className='back'>
                 <Container>
-                    <div id='section-color'>
 
                     <Row className='justify-content-center'>
                         <Col xs={12}>
@@ -43,95 +42,7 @@ export default function Footer() {
                         </a>
                     </Col>
 
-  const { t } = useTranslation();
-  return (
-    <>
-      <section className="back">
-        <Container>
-          <Row className="justify-content-center">
-            <Col xs={12}>
-              <hr></hr>
-              <h5>{t("text.footer_turboMax")}</h5>
-              <p>{t("text.turboMax")}</p>
-              <div className="stoir">
-                <img src={star} alt="" className="strix" />
-                <h6 className="footer_h6">{t("text.iTech")}</h6>
-              </div>
-            </Col>
-          </Row>
-          <Row className="justify-content-center mt-5">
-            <Col xs={1} className="iconchalar">
-              <a href="https://github.com/MuhammadazizEsonboyev/translate-app-turbomax">
-                <i class="fa-brands fa-github"></i>
-              </a>
-            </Col>
-            <Col xs={1} className="iconchalar">
-              <a href="https://t.me/itechacademy_uz">
-                <i class="fa-brands fa-telegram"></i>
-              </a>
-            </Col>
-            <Col xs={1} className="iconchalar">
-              <a href="https://www.youtube.com/@itechcompany8336">
-                <i class="fa-brands fa-youtube"></i>
-              </a>
-            </Col>
 
-                        <Col xs={3}>
-                            <ul className='list'>
-                                <Accordion defaultActiveKey="1">
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header>Responsive</Accordion.Header>
-                                        <Accordion.Body>
-                                            <li>Asadbek</li>
-                                            <br></br>
-                                            <li>Xurshid</li>
-                                            <br></br>
-                                            <li>Abdumurod</li>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </ul>
-                        </Col>
-                        <Col xs={3}>
-                            <ul className='list'>
-                                <Accordion defaultActiveKey="1">
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header>Tarjima</Accordion.Header>
-                                        <Accordion.Body>
-                                            <li>Samandar</li>
-                                            <br></br>
-                                            <li>Bobur</li>
-                                            <br></br>
-                                            <li>Asadbek</li>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </ul>
-                        </Col>
-                        <Col xs={3}>
-                            <ul className='list'>
-                                <Accordion defaultActiveKey="1">
-                                    <Accordion.Item eventKey="0">
-                                        <Accordion.Header >Dizayn</Accordion.Header>
-                                        <Accordion.Body>
-                                            <li>Doston</li>
-                                            <br></br>
-                                            <li>Bekhruz</li>
-                                            <br></br>
-                                            <li>Sanjar</li>
-                                        </Accordion.Body>
-                                    </Accordion.Item>
-                                </Accordion>
-                            </ul>
-                        </Col>
-                        <hr></hr>
-                    </Row>
-
-                    </div>
-                </Container>
-            </section>
-        </>
-    )
                       
             <Col xs={3}>
               <ul className="list" >
