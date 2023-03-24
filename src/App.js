f229e4193f8f321a2061906d59f08ba71ca95f32
sotiboldiyevxurshid <sotiboldiyevxurshid21@gmail.com>
2023-03-24 11:46:27 +0500

    g

--- src/App.js
+++ src/App.js
@@ -22,17 +22,15 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
+
       <NavbarPage /> 
       <Cardstorage />
 
 
-=======
       <NavbarPage/>
       <Cardstorage/>
       <Footer/>
   
->>>>>>> 07ceee53c658cbef1389701cbb1763cc13d101ab
     </>
   );
 }
